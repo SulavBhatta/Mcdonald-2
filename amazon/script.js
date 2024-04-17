9cbd7da1c0ce4712493c66b8edb6248ba73b8a02
SulavBhatta <166143775+SulavBhatta@users.noreply.github.com>
2024-04-17 20:44:51 +0900

    Add files via upload

---
+++ amazon/script.js
@@ -0,0 +1,10 @@
+function showSlideBar(){
+    const slidebar = document.querySelector(".slidebar");
+    slidebar.style.display = "flex";
+}
+
+function hideSlidebar(){
+    const slidebar = document.querySelector(".slidebar");
+    slidebar.style.display = "none";
+}
+
